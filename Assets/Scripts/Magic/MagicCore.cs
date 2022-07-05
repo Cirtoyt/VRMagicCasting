32a2cb3015d5c6964b8599347e29de564be0fba8
Conrad Wilson <conradjwilson@hotmail.com>
2022-07-05 21:28:26 +0100

    Added basic fireball magic
    +Added MagicCore script.
    +Added basic fireball prefab with a simple particle effect.

---
+++ Assets/Scripts/Magic/MagicCore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCore : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
